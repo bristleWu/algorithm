9966060526f7ec73147dc60ae605b4f13df9fbab
bristleWu <61814464+bristleWu@users.noreply.github.com>
2020-09-12 23:45:33 +0800

    init

---
+++ src/com/bristle/sort/QuickSort.java
@@ -0,0 +1,16 @@
+package com.bristle.sort;
+
+/**
+ * @author GouWei
+ * @version 1.0
+ * @description 快速排序
+    1. 确定分界点 x -> left/mid/right
+    2. 调整位置,使: 左边 <= x 右边 >= x n
+    3. 递归处理左右两边                 logn
+
+ * @date 2020-09-12 23:16
+ */
+public class QuickSort {
+
+
+}
