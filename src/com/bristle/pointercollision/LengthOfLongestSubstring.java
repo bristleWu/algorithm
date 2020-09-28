a81b8436884da16131cd20d340a5c71d51e185ef
狗尾 <gouwei@jushuitan.com>
2020-09-28 21:24:31 +0800

    最长连续子字符串

--- src/com/bristle/pointercollision/LengthOfLongestSubstring.java
+++ src/com/bristle/pointercollision/LengthOfLongestSubstring.java
@@ -7,4 +7,17 @@ package com.bristle.pointercollision;
  * @date 2020-09-28 21:12
  */
 public class LengthOfLongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        int[] chars = new int[128];
+        int j = 0, res = 0;
+        for(int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i)]++;
+            while(chars[s.charAt(i)] > 1) {
+                chars[s.charAt(j++)]--;
+            }
+            res = Math.max(res,i - j + 1);
+        }
+        return res;
+    }
 }
